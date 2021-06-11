2a426f926d0775691a87472f3d591b9d6186c91d
erlichchen <chenxuewei.dev@gmail.com>
2021-06-11 09:27:57 +0800

    ESB-19 Init Project

---
+++ src/main/java/com/ccc/example/ExampleJavaServerBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.ccc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJavaServerBoilerplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleJavaServerBoilerplateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ccc/example/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ccc.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloHandler()  {
+        return "hello spring boot";
+    }
+}
